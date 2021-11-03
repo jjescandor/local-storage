9f604809e3a78bd52a59af612d589a94b900ab3a
JJ Escandor <jj.p.escandor@gmail.com>
2021-11-03 19:19:17 -0400

    implemented local storage

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const nameInput = studentForm['name'];
 const ageInput = studentForm['age'];
 const rollInput = studentForm['roll'];
 
-const students = [];
+const students = JSON.parse(localStorage.getItem("students")) || [];
 
 const addStudent = (name, age, roll) => {
     students.push({
@@ -15,6 +15,7 @@ const addStudent = (name, age, roll) => {
         age,
         roll,
     });
+    localStorage.setItem("students", JSON.stringify(students));
     return { name, age, roll }
 }
 
